a20d3d356ed4cb42c8259ec584fb14e117c7fabf
Elton Freitas <eltons.freitas01@gmail.com>
2021-05-04 18:19:08 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <h1>Olá Mundo!</h1>
+    <div>
+       <h1 className="text-primary">Olá Mundo!</h1>
     </div>
   );
 }
